735bcae00add3b980d1046b82c65998ff9023310
Yash Patel <yashpatel1815@hotmail.com>
2024-02-26 20:37:34 -0500

    Changed styling of modal

--- frontend/src/components/UploadPFPModal.js
+++ frontend/src/components/UploadPFPModal.js
@@ -1,29 +1,30 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 export const UploadPFPModal = () => {
   return (
-    <div
-      className="modal show"
-      style={{ display: "block", position: "initial" }}
-    >
-      <Modal.Dialog>
-        <Modal.Header closeButton>
-          <Modal.Title>Upload Profile Picture</Modal.Title>
-        </Modal.Header>
+      <div
+        className="modal show"
+        style={{ display: "block", position: "initial", width: "75%", margin: "auto"}}
+      >
+        <Modal.Dialog>
+          <Modal.Header closeButton>
+            <Modal.Title>Upload Profile Picture</Modal.Title>
+          </Modal.Header>
 
-        <Modal.Body>
-          <div className="text-center">
-          <p>Upload Profile Picture</p>
-          <p>Supported format: .jpeg .png</p>
-          </div>
-        </Modal.Body>
+          <Modal.Body>
+            <div className="text-center p-5" style={{border: "2px dashed #485785"}}>
+              <p>Drop your picture here</p>
+              <p>Supported format: .jpeg .png</p>
+            </div>
+          </Modal.Body>
 
-        <Modal.Footer>
-          <Button variant="secondary">close</Button>
-          <Button variant="primary">Save</Button>
-        </Modal.Footer>
-      </Modal.Dialog>
-    </div>
+          <Modal.Footer>
+            <Button variant="secondary">close</Button>
+            <Button variant="primary">Save</Button>
+          </Modal.Footer>
+        </Modal.Dialog>
+      </div>
   );
 };
