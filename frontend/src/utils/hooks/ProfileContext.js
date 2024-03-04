6c12547ab68a5eb5663d3d23c8c29be511754337
JRB958 <141108231+JRB958@users.noreply.github.com>
2024-03-03 20:35:26 -0500

    add a check for user role before fetching from a specific endpoint #60

--- frontend/src/utils/hooks/ProfileContext.js
+++ frontend/src/utils/hooks/ProfileContext.js
@@ -23,31 +23,71 @@ export function ProfileProvider(props) {
     });
 
     const getProfileInformation = async () => {
+        let role = ''
         const id = localStorage.getItem("ID");
         axiosInstance
-            .get(`profiles/public-profile/${id}/`)
+            .get(`profiles/user/${id}/`)
             .then((response) => {
-                console.log(response);
-                setProfileInfo({
-                    //TODO check where the photo is? user vs profile
-                    // avatar: response.data.profile_photo,
-
-                    // get information from the user model
-                    first_name: response.data.user.first_name,
-                    last_name: response.data.user.last_name,
-                    email: response.data.user.email,
-                    // get information from the public-profile model
-                    phone_number: response.data.phone_number,
-                    address: response.data.address,
-                    city: response.data.city,
-                    province: response.data.province,
-                    postal_code: response.data.postal_code,
-                });
-                console.log(response.data);
+                role = response.data.role;
+                console.log(role);
+                if (role == "COMPANY") {
+                    axiosInstance
+                        .get(`profiles/company-profile/${id}/`)
+                        .then((response) => {
+                            console.log(response);
+                            setProfileInfo({
+                                //TODO check where the photo is? user vs profile
+                                // avatar: response.data.profile_photo,
+        
+                                // get information from the user model
+                                first_name: response.data.user.first_name,
+                                last_name: response.data.user.last_name,
+                                email: response.data.user.email,
+                                // get information from the public-profile model
+                                phone_number: response.data.phone_number,
+                                address: response.data.address,
+                                city: response.data.city,
+                                province: response.data.province,
+                                postal_code: response.data.postal_code,
+                            });
+                            console.log(response.data);
+                        })
+                        .catch((error) => {
+                            console.error("Error fetching user profile:", error.message);
+                        });
+                } else if (role == "PUBLIC") {
+                    axiosInstance
+                        .get(`profiles/public-profile/${id}/`)
+                        .then((response) => {
+                            console.log(response);
+                            setProfileInfo({
+                                //TODO check where the photo is? user vs profile
+                                // avatar: response.data.profile_photo,
+        
+                                // get information from the user model
+                                first_name: response.data.user.first_name,
+                                last_name: response.data.user.last_name,
+                                email: response.data.user.email,
+                                // get information from the public-profile model
+                                phone_number: response.data.phone_number,
+                                address: response.data.address,
+                                city: response.data.city,
+                                province: response.data.province,
+                                postal_code: response.data.postal_code,
+                            });
+                            console.log(response.data);
+                        })
+                        .catch((error) => {
+                            console.error("Error fetching user profile:", error.message);
+                        });
+                }
             })
             .catch((error) => {
                 console.error("Error fetching user profile:", error.message);
             });
+
+        
+
     }
 
     const setProfileInformation = (profile) => {
