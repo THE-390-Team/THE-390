831b734d88144d9ba9c8e63c96e705efd02d2f88
patrickmac3 <patrickmaceachen9@gmail.com>
2024-03-16 18:11:17 -0400

    Add ParkingRegistrationKeyView to urls.py and views.py

--- backend/registration_key/urls.py
+++ backend/registration_key/urls.py
@@ -1,8 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import CondoRegistrationKeyView
+
+
+from .views import CondoRegistrationKeyView, ParkingRegistrationKeyView
 router = DefaultRouter()
 
 router.register(r'condo-registration-key', CondoRegistrationKeyView, basename="condo-registration-key" )
+router.register(r'parking-registration-key', ParkingRegistrationKeyView, basename="parking-registration-key" )
 
 
 urlpatterns = router.urls 
