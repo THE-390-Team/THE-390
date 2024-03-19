44300e676fdee2549c5f9537b6f3caae8869a35c
Vithu <vithujanan0629@gmail.com>
2024-03-18 20:10:26 -0400

    Adding a title to the Home screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,10 +4,14 @@ import CarouselHomeScreen from "./homeScreenCarousel/CarouselHomeScreen"
 
 
 const HomeScreen = () => {
+
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-lg-10 col-md-13 mt-5">
+                   <div className = "d-flex flex-column align-items-center">
+                     <h1 style = {{fontSize: "50px", marginBottom: "30px", fontWeight: "bold" }}>CondoCare</h1>
+                    </div>
                     <CarouselHomeScreen />
                 </div>
             </div>
