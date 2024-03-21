50bc99f1c8bff022861cc53a889c26c8f4ab842f
JRB958 <141108231+JRB958@users.noreply.github.com>
2024-03-21 01:25:54 -0400

    further changes to drop it below previous 9%

--- frontend/cypress/e2e/PropertyTesting.cy.js
+++ frontend/cypress/e2e/PropertyTesting.cy.js
@@ -40,26 +40,11 @@ describe('Sign in as company and Navigate to Create property and create condo un
     cy.contains('Dashboard').click();
     cy.contains('Profile').click();
     cy.contains('Dashboard').click();
-    cy.get('[data-testid="create-property-button"]').click();
-    cy.url().should('include', '/create-property');
-    cy.get('[data-testid="property-name-input"]').type('test')
-    cy.get('[data-testid="property-address-input"]').type('test')
-    cy.get('[data-testid="property-city-input"]').type('test')
-    cy.get('[data-testid="province-select-test"]').select("QC")
-    cy.get('[data-testid="property-city-input"]').type('test')
-    cy.get('[data-testid="property-postal-code-input"]').type('H3H3H3')
-    cy.get('[data-testid="submit-button"]').click()
+    cy.createProperty();
   })
   it('Navigate to create condo unit from property page and creates a condo unit', () => {
     cy.goToProperty3();
-    cy.get('[data-testid="create-condo-unit-button"]').click()
-    cy.get('[data-testid="unit-location-input"]').type('1234')
-    cy.get('[data-testid="unit-size-input"]').type('1000')
-    cy.get('[data-testid="unit_purchase_price-input"]').type('1000')
-    cy.get('[data-testid="unit_rental_price-input"]').type('1000')
-    cy.get('[data-testid="unit-info-input"]').type('random info')
-    cy.get('[data-testid="submit-button"]').click()
-    cy.url().should('include', '/property-page')
+    cy.createCondoUnit();
   })
 })
 
@@ -69,14 +54,7 @@ describe('Sign in as company and Navigate to create a parking unit', () => {
   })
   it('Navigate to create parking unit from property page', () => {
     cy.goToProperty3();
-    cy.get('[data-testid="create-parking-unit-button"]').click()
-    cy.get('[data-testid="parking-location-input"]').type('1234')
-    cy.get('[data-testid="parking-size-input"]').type('1000')
-    cy.get('[data-testid="parking-purchase_price-input"]').type('1000')
-    cy.get('[data-testid="parking-rent_price-input"]').type('1000')
-    cy.get('[data-testid="parking-extra_information-input"]').type('random info')
-    cy.get('[data-testid="submit-button"]').click()
-    cy.url().should('include', '/property-page')
+    cy.createParkingUnit();
   })
 })
 
@@ -86,14 +64,7 @@ describe('Sign in as company and Navigate to create a storage unit', () => {
   })
   it('Navigate to create parking unit from property page', () => {
     cy.goToProperty3();
-    cy.get('[data-testid="create-storage-unit-button"]').click()
-    cy.get('[data-testid="locker-location-input"]').type('1234')
-    cy.get('[data-testid="locker-size-input"]').type('1000')
-    cy.get('[data-testid="locker-purchase_price-input"]').type('1000')
-    cy.get('[data-testid="locker-rent_price-input"]').type('1000')
-    cy.get('[data-testid="locker-extra_information-input"]').type('random info')
-    cy.get('[data-testid="submit-button"]').click()
-    cy.url().should('include', '/property-page')
+    cy.createStorageUnit();
   })
 })
 
