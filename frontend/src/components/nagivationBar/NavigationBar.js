4a9e9122c6b053b1bc8e629c5ff09e67d1acd8df
JRB958 <141108231+JRB958@users.noreply.github.com>
2024-03-21 18:06:48 -0400

    fix some issues from merging

--- frontend/src/components/nagivationBar/NavigationBar.js
+++ frontend/src/components/nagivationBar/NavigationBar.js
@@ -3,10 +3,12 @@ import { useAuth } from "../../utils/hooks/AuthContext";
 import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { useLocation } from "react-router-dom";
+import { useProfile } from '../../utils/hooks/ProfileContext';
 
 const NavigationBar = () => {
   const { isLoggedIn } = useAuth();
   const location = useLocation();  // Using useLocation to access the current path
+  const { role } = useProfile();
 
   const getActiveKey = () => {
     // This function determines the active key based on the current pathname
@@ -37,8 +39,7 @@ const NavigationBar = () => {
               </LinkContainer>}
             </Nav>
             <Nav className="ms-auto justify-content-end" variant="tabs" activeKey={getActiveKey()}>
-              {isLoggedIn ?
-
+              {isLoggedIn && role === "COMPANY" ?
                 <Nav.Item evenKey="/operation">
                   <LinkContainer to="/operation">
                     <Nav.Link>
