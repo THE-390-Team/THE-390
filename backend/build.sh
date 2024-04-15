ccf298b56cbca80df8f8eef90c259e371eedcad1
patrickmac3 <patrickmaceachen9@gmail.com>
2024-04-15 12:26:14 -0400

    Update email address for superuser creation in build.sh script

--- backend/build.sh
+++ backend/build.sh
@@ -13,23 +13,15 @@ echo "Creating superuser..."
 
 
 # Provide specific information for the superuser
-email="admin@example.com"
+email="admin@gmail.com"
 role="PUBLIC"
 first_name="Admin"
 last_name="User"
 password="adminpassword123"
 
-
 echo "Attempting to create superuser..."
 if python manage.py createsuperuser --email "$email" --role "$role" --first_name "$first_name" --last_name "$last_name" --noinput; then
     echo "Superuser created successfully!"
 else
-    # Check if the error is due to the email already being in use
-    if [[ $? -ne 0 ]] && [[ $(python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(email='$email').exists())") == "True" ]]; then
-        echo "Email '$email' is already in use. Skipping superuser creation."
-    else
-        # If the error is not due to email conflict, exit with error
-        echo "Failed to create superuser."
-        exit 1
-    fi
+    echo "Failed to create superuser, but continuing..."
 fi
