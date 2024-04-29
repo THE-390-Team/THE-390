b27b9e7c2dae17a56deded496238c119e8610f99
patrickmac3 <patrickmaceachen9@gmail.com>
2024-04-29 17:58:05 -0400

    Add user profile and company finance tests

--- backend/user_profile/tests.py
+++ backend/user_profile/tests.py
@@ -4,7 +4,8 @@ from rest_framework import status
 from django.urls import reverse
 from .serializers import UserSerializer
 from .models import User, PublicProfile, EmployeeProfile, CompanyProfile
-
+from properties.models import PropertyProfile, CondoUnit, ParkingUnit, StorageUnit
+from rest_framework.test import APIClient
 UserModel = get_user_model()
 
 
@@ -161,3 +162,160 @@ class TestUserSignUp(TestCase):
         })
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data["password"],["This field may not be blank."])
+        
+        
+        
+class CompanyFinanceViewTestCase(TestCase):
+    
+
+    @classmethod
+    def setUpTestData(cls):
+    
+        
+        cls.company_user = User.objects.create_user(
+            email='company@example.com',
+            password='password',
+            role=User.Role.COMPANY,
+            first_name='John',
+            last_name='Doe'
+        )
+        cls.company = CompanyProfile.objects.get(
+            user=cls.company_user
+        )
+        
+        cls.public_user = User.objects.create_user(
+            email='public@example.com',
+            password='password',
+            role=User.Role.PUBLIC,
+            first_name='Public',
+            last_name='User'
+        )
+        cls.public = PublicProfile.objects.get(user=cls.public_user)
+        
+        cls.property = PropertyProfile.objects.create(
+            company=cls.company,
+            name='Property 1',
+            address='123 Test St',
+            city='Test City',
+            province='Test Province',
+            postal_code='T3S T1N',
+            fee_rate=0.05
+        )
+        
+        cls.condo = CondoUnit.objects.create(
+            property=cls.property, 
+            location='2',
+            operational_expense=100,
+            purchase_price=100000, 
+            rent_price=1000,
+            public_profile=cls.public,
+            size=1000, 
+
+        )
+        cls.parking = ParkingUnit.objects.create(
+            property=cls.property,
+            location='2',
+            operational_expense=50,
+            purchase_price=100000, 
+            rent_price=1000,
+            public_profile=cls.public,
+            size=1000
+
+        )
+        cls.storage = StorageUnit.objects.create(
+            property=cls.property,
+            location='2',
+            operational_expense=75,
+            purchase_price=100000, 
+            rent_price=1000,
+            public_profile=cls.public,
+            size=1000, 
+
+        )
+        
+  
+
+    def test_get_company_finance(self):
+        
+        
+        
+        
+        
+        # TOTAL FOR CONDO 
+        condo_budget = []
+        total_condo_expenses = 0
+        total_condo_fees = 0
+        
+        total_condo_expenses += self.condo.operational_expense
+        condo_data ={
+            "id": self.condo.id,
+            "condo": self.condo.location,
+            "expense": self.condo.operational_expense,
+            "fee": self.condo.property_fee
+        }
+        total_condo_fees += self.condo.property_fee
+        condo_budget.append(condo_data)
+        
+        
+        # TOTAL FOR PARKING
+        total_parking_expenses = 0
+        total_parking_fees = 0
+        parking_budget = []
+        
+        total_parking_expenses += self.parking.operational_expense
+        parking_data ={
+            "id": self.parking.id,
+            "parking": self.parking.location,
+            "expense": self.parking.operational_expense,
+            "fee": self.parking.property_fee
+        }
+        total_parking_fees += self.parking.property_fee 
+        parking_budget.append(parking_data)
+        
+        # TOTAL FOR STORAGE
+        total_storage_expenses = 0
+        total_storage_fees = 0
+        storage_budget = []
+        storage_data = {
+            "id": self.storage.id,
+            "storage": self.storage.location,
+            "expense": self.storage.operational_expense,
+            "fee": self.storage.property_fee
+        }
+        total_storage_fees += self.storage.property_fee
+        total_storage_expenses += self.storage.operational_expense
+        storage_budget.append(storage_data)
+        
+           
+        
+        
+        company_total_fees = total_condo_fees + total_parking_fees + total_storage_fees
+        company_total_expenses = total_condo_expenses + total_parking_expenses + total_storage_expenses
+        data = {
+            f'{self.property.id}': {
+              "property_name": self.property.name,
+                "condos": condo_budget,
+                "parkings": parking_budget,
+                "storages": storage_budget,
+                "condo_fee": total_condo_fees,
+                "parking_fee": total_parking_fees,
+                "storage_fee": total_storage_fees,
+                "condo_expense": total_condo_expenses,
+                "parking_expense": total_parking_expenses,
+                "storage_expense": total_storage_expenses,
+                "fee": total_condo_fees + total_parking_fees + total_storage_fees,
+                "expenses": total_condo_expenses + total_parking_expenses + total_storage_expenses
+            }
+        }
+        
+        expected_response = {
+            "properties": data, 
+            "expenses": company_total_expenses,
+            "fee": company_total_fees, 
+            "total": company_total_fees - company_total_expenses
+        }
+        url = reverse('company-finance', kwargs={'company_id': self.company.user_id})
+        response = self.client.get(url)
+        self.assertDictEqual(response.data, expected_response)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
