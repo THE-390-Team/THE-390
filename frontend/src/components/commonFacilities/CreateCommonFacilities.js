2bec6937cea65a2fb53bf81487e956f0e7ac6b2d
JRB958 <141108231+JRB958@users.noreply.github.com>
2024-04-30 15:13:55 -0400

    update the CreateCommonFacilities.js to match more the backend models

--- frontend/src/components/commonFacilities/CreateCommonFacilities.js
+++ frontend/src/components/commonFacilities/CreateCommonFacilities.js
@@ -2,20 +2,13 @@ import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axiosInstance from "../../api/axios";
 import { useProperty } from "../../utils/hooks/PropertyContext"
-import {
-  Container,
-  Row,
-  Col,
-  Button,
-  Form,
-  Card,
-  ListGroup
-} from "react-bootstrap";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
 import LargeTitle from "../LargeTitle.js";
 import { useEffect } from "react"
 
 const CreateCommonFacilities = () => {
     const { propertyId } = useParams();
+    const { createFacility, fetchAllFacilities, facilities } = useProperty();
 
     let navigate = useNavigate();
 
@@ -25,26 +18,27 @@ const CreateCommonFacilities = () => {
         type: "Gym",
         description: "",
         capacity: "",
-        reservation_duration: "",
+        reservation_duration: "", // this is technically the start
+        reservation_duration_end: "",
     });
 
     const [errors, setErrors] = useState({});
-    
+
     //handle change from the user input and save to state
     const handleChange = (e) => {
         setFormData({
-          ...formData,
-          // Trimming any whitespace
-          [e.target.name]: e.target.value,
+            ...formData,
+            // Trimming any whitespace
+            [e.target.name]: e.target.value,
         });
-    
+
         //Clear existing error if there is change to input
         setErrors({ ...errors, [e.target.name]: '' });
         console.log(formData);
-      };
-      
+    };
+
     //handle form submission to create a new property
-    const validateForm = () =>{
+    const validateForm = () => {
         const errors = {};
         let isValid = true;
 
@@ -73,68 +67,52 @@ const CreateCommonFacilities = () => {
         } else if (isNaN(parseInt(formData.capacity))) {
             errors.capacity = "Your capacity must consist of numbers only";
             isValid = false;
-        } else if (parseInt(formData.capacity) > 100){
-            errors.capacity  = "Max Capacity Allowed: 100"
+        } else if (parseInt(formData.capacity) > 100) {
+            errors.capacity = "Max Capacity Allowed: 100"
         }
 
-        // Reservation duration must be filled, must consist only of numbers, and must be less than 1440
+        // Start Time Validation (hh:mm:ss in 24-hour format)
         if (!formData.reservation_duration) {
-            errors.reservation_duration = "Please enter facility reservation duration";
+            errors.reservation_duration = "Please enter start time";
             isValid = false;
-        } else if (isNaN(parseInt(formData.reservation_duration))) {
-            errors.reservation_duration = "Your reservation duration must consist of numbers only";
+        } else if (!/^([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/.test(formData.reservation_duration)) {
+            errors.reservation_duration = "Start time must be in the format of hh:mm:ss";
             isValid = false;
-        } else if (parseInt(formData.reservation_duration) > 1440){
-            errors.reservation_duration  = "Max reservation duration allowed: 1440 minutes"
         }
+
+        // End Time Validation (hh:mm:ss in 24-hour format)
+        if (!formData.reservation_duration_end) {
+            errors.reservation_duration_end = "Please enter end time";
+            isValid = false;
+        } else if (!/^([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/.test(formData.reservation_duration_end)) {
+            errors.reservation_duration_end = "End time must be in the format of hh:mm:ss";
+            isValid = false;
+        }
+
 
         //If there are errors, set errors in state and prevent submit
         if (Object.keys(errors).length > 0) {
             setErrors(errors);
         }
-    
+
         return isValid;
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-    
+        console.log("clicked submit")
+
         //If form is valid, post the form
         if (validateForm()) {
-          console.log(formData);
-    
-          //post form
-          axiosInstance
-
-          // TODO: Check for endpoint, as this might change
-            .postForm(`properties/property-profile/${propertyId}/common-facilities/`, {
-              name: formData.name,
-              type: formData.type,
-              description: formData.description,
-              capacity: formData.capacity,
-              reservation_duration: formData.reservation_duration,
-            })
-            .then((res) => {
-              if (res.status == 201) {
-                // Create new property if successful and go back to property dashboard
-                window.alert(`Common Facility ${formData.name} has been created`)
-                console.log(res);
-                console.log(res.data);
-                goBack();
-              }
-            })
-            .catch((error) => {
-              //Show popup of error encountered
-              console.log(error);
-              console.log(error.data);
-              window.alert(`${error} `)
-            });
+            console.log(formData);
+            console.log("clicked submit after validation")
+            createFacility(formData, propertyId, goBack)
         } else {
-          //Do not post form if there is error in input
-          return;
+            //Do not post form if there is error in input
+            return;
         }
     };
-    
+
     //Go back to previous page
     function goBack() {
         navigate(-1);
@@ -146,93 +124,107 @@ const CreateCommonFacilities = () => {
                 {/* a page title */}
                 <LargeTitle title="Create Common Facilities" />
             </Row>
-                <Form className="py-5 text-dark" onSubmit={handleSubmit}>
+            <Form className="py-5 text-dark" onSubmit={handleSubmit}>
                 {/*TODOchange to name instead of id*/}
                 <Row className="mb-3">
-                
-                <Form.Group as={Col} controlId="formGridName">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                    data-testid="facility-name"
-                    name="name"
-                    onChange={handleChange}
-                    placeholder="Enter Facility Name"
-                    type="text"
-                    value={formData.name}
-                    />
-                    {/*Show error if submitting invalid input*/}
-                    {errors.name && <span style={{ color: "red" }}>{errors.name}</span>}
-                </Form.Group>
-                
-                <Form.Group as={Col} controlId="formGridType">
-                    <Form.Label>Facility Type</Form.Label>
-                    <Form.Select
-                    defaultValue={formData.property_rovince}
-                    name="type"
-                    value={formData.type}
-                    onChange={handleChange}
-                    data-testid="type-select-test">
-                    <option value="Gym" data-testid="gym-test">Gym</option>
-                    <option value="Swimming Pool" data-testid="SP-test">Swimming Pool</option>
-                    <option value="Meeting Room" data-testid="MR-test">Meeting Room</option>
-                    <option value="Lounge" data-testid="lounge-test">Lounge</option>
-                    </Form.Select>
-                </Form.Group>
-            </Row>
 
-            <Row>
-                <Form.Group as={Col} controlId="formGridDescription">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control
-                    data-testid="facility-description"
-                    name="description"
-                    onChange={handleChange}
-                    placeholder="Enter Facility Description"
-                    type="text"
-                    value={formData.description}
-                    />
-                    {/*Show error if submitting invalid input*/}
-                    {errors.description && <span style={{ color: "red" }}>{errors.description}</span>}
-                </Form.Group>
-            </Row>
+                    <Form.Group as={Col} controlId="formGridName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            data-testid="facility-name"
+                            name="name"
+                            onChange={handleChange}
+                            placeholder="Enter Facility Name"
+                            type="text"
+                            value={formData.name}
+                        />
+                        {/*Show error if submitting invalid input*/}
+                        {errors.name && <span style={{ color: "red" }}>{errors.name}</span>}
+                    </Form.Group>
 
-            <Row>
-                <Form.Group as={Col} controlId="formGridCapacity">
-                    <Form.Label>Capacity</Form.Label>
-                    <Form.Control
-                    data-testid="facility-capacity"
-                    name="capacity"
-                    onChange={handleChange}
-                    placeholder="Enter Facility Capacity"
-                    type="text"
-                    value={formData.capacity}
-                    />
-                    {/*Show error if submitting invalid input*/}
-                    {errors.capacity && <span style={{ color: "red" }}>{errors.capacity}</span>}
-                </Form.Group>
+                    <Form.Group as={Col} controlId="formGridType">
+                        <Form.Label>Facility Type</Form.Label>
+                        <Form.Select
+                            defaultValue={formData.property_rovince}
+                            name="type"
+                            value={formData.type}
+                            onChange={handleChange}
+                            data-testid="type-select-test">
+                            <option value="Gym" data-testid="gym-test">Gym</option>
+                            <option value="Swimming Pool" data-testid="SP-test">Swimming Pool</option>
+                            <option value="Meeting Room" data-testid="MR-test">Meeting Room</option>
+                            <option value="Lounge" data-testid="lounge-test">Lounge</option>
+                        </Form.Select>
+                    </Form.Group>
+                </Row>
 
-                <Form.Group as={Col} controlId="formGridCapacity">
-                    <Form.Label>Reservation Duration</Form.Label>
-                    <Form.Control
-                    data-testid="facility-reservation"
-                    name="reservation_duration"
-                    onChange={handleChange}
-                    placeholder="Enter Facility Reservation Duration"
-                    type="text"
-                    value={formData.reservation_duration}
-                    />
-                    {/*Show error if submitting invalid input*/}
-                    {errors.reservation_duration && <span style={{ color: "red" }}>{errors.reservation_duration}</span>}
-                </Form.Group>
-            </Row>
+                <Row>
+                    <Form.Group as={Col} controlId="formGridDescription">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            data-testid="facility-description"
+                            name="description"
+                            onChange={handleChange}
+                            placeholder="Enter Facility Description"
+                            type="text"
+                            value={formData.description}
+                        />
+                        {/*Show error if submitting invalid input*/}
+                        {errors.description && <span style={{ color: "red" }}>{errors.description}</span>}
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formGridCapacity">
+                        <Form.Label>Capacity</Form.Label>
+                        <Form.Control
+                            data-testid="facility-capacity"
+                            name="capacity"
+                            onChange={handleChange}
+                            placeholder="Enter Facility Capacity"
+                            type="text"
+                            value={formData.capacity}
+                        />
+                        {/*Show error if submitting invalid input*/}
+                        {errors.capacity && <span style={{ color: "red" }}>{errors.capacity}</span>}
+                    </Form.Group>
+                </Row>
 
-            <Button style={{ marginTop: "20px" }} variant="primary" onClick={goBack}>
-                Cancel
-            </Button>
-            <Button style={{ marginTop: "20px", marginLeft: "20px" }} variant="primary" type="submit" data-testid="submit-button">
-                Submit
-            </Button>
-        </Form>
+                <Row>
+
+
+                    <Form.Group as={Col} controlId="formGridStartTime">
+                        <Form.Label>Start Time</Form.Label>
+                        <Form.Control
+                            data-testid="facility-reservation"
+                            name="reservation_duration"
+                            onChange={handleChange}
+                            placeholder="Enter Start Time"
+                            type="text"
+                            value={formData.reservation_duration}
+                        />
+                        {/*Show error if submitting invalid input*/}
+                        {errors.reservation_duration && <span style={{ color: "red" }}>{errors.reservation_duration}</span>}
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formGridEndTime">
+                        <Form.Label>End Time</Form.Label>
+                        <Form.Control
+                            data-testid="facility-reservation_end"
+                            name="reservation_duration_end"
+                            onChange={handleChange}
+                            placeholder="Enter End Time"
+                            type="text"
+                            value={formData.reservation_end}
+                        />
+                        {/*Show error if submitting invalid input*/}
+                        {errors.reservation_duration && <span style={{ color: "red" }}>{errors.reservation_duration}</span>}
+                    </Form.Group>
+                </Row>
+
+                <Button style={{ marginTop: "20px" }} variant="primary" onClick={goBack}>
+                    Cancel
+                </Button>
+                <Button style={{ marginTop: "20px", marginLeft: "20px" }} variant="primary" type="submit" data-testid="submit-button">
+                    Submit
+                </Button>
+            </Form>
         </Container>
     );
 }
