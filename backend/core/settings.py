a82da710a783d8e105b7bf29d896ccd7f0c7a857
patrickmac3 <patrickmaceachen9@gmail.com>
2024-04-30 15:59:18 -0400

    Update email settings to use environment variables for Mailtrap configuration

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -204,16 +204,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'patrickmaceachen78@gmail.com'
-# EMAIL_HOST_PASSWORD = 'uvjx lzqn mrbb xgur'
 EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
-EMAIL_HOST_USER = '7999797806cac8'
-EMAIL_HOST_PASSWORD = 'f20389bed43185'
-EMAIL_PORT = '2525'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+EMAIL_USE_SSL = env('EMAIL_USE_SSL')
 
 # Default location for images and reference URL
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
