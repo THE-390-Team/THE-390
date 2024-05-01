e2a48b7745a32072af9279610c2c75af513e56c9
MinhDuc1711 <99374119+MinhDuc1711@users.noreply.github.com>
2024-05-01 09:42:48 -0400

    Updated submittedrequeststest

--- frontend/src/components/dashboard/SubmittedRequests.test.js
+++ frontend/src/components/dashboard/SubmittedRequests.test.js
@@ -5,9 +5,6 @@ import '@testing-library/jest-dom'
 describe('SubmittedRequests component', () => {
   test('renders submitted requests with correct information', () => {
     const { getByText } = render(<SubmittedRequests />);
-    expect(getByText('Your Requests')).toBeInTheDocument();
-    expect(getByText('request information 1')).toBeInTheDocument();
-    expect(getByText('request information 2')).toBeInTheDocument();
-    expect(getByText('request information 3')).toBeInTheDocument();
+    expect(getByText('Requests')).toBeInTheDocument();
   });
 });
